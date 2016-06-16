5370f8604ca78f3ff25e2ef8a5e6d6974a40166f
Scott Keith Thomas, Jr. <sthomas@idis.com>
2016-06-15 23:56:55 -0600

    Added sample console.

---
+++ Source/ExtensibleServiceProcess.SampleConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+
+namespace ExtensibleServiceProcess.SampleConsole
+{
+    internal class Service : ExtensibleServiceBase
+    {
+        private Service()
+        {
+            AllowMultipleServiceStarts = false;
+            AutoLog = true;
+            CanHandlePowerEvent = false;
+            CanHandleSessionChangeEvent = false;
+            CanPauseAndContinue = false;
+            CanShutdown = false;
+            CanStop = true;
+            ExitCode = 0;
+            ServiceName = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title;
+            ServiceDescription = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+            ServiceDisplayName = "Sample Service";
+        }
+
+        internal static int Main(string[] args)
+        {
+            using (var service = new Service())
+            {
+                Console.Title = "Sample Console";
+                Console.CursorVisible = false;
+
+                if (Environment.UserInteractive)
+                {
+                    service.OnStart(args);
+                    service.OnStop();
+                }
+                else
+                {
+                    Run(service);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+            return 0;
+        }
+    }
+}
--- Source/ExtensibleServiceProcess.SampleConsole/Properties/AssemblyInfo.cs
+++ Source/ExtensibleServiceProcess.SampleConsole/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ExtensibleServiceProcess")]
-[assembly: AssemblyDescription("ExtensibleServiceProcess is a library to support an extensible service application.")]
+[assembly: AssemblyTitle("ExtensibleServiceProcess.SampleConsole")]
+[assembly: AssemblyDescription("ExtensibleServiceProcess sample console/service.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Scott Keith Thomas, Jr.")]
-[assembly: AssemblyProduct("ExtensibleServiceProcess")]
+[assembly: AssemblyProduct("ExtensibleServiceProcess.SampleConsole")]
 [assembly: AssemblyCopyright("Copyright © 2016 Scott Keith Thomas, Jr.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("0130cefb-405f-4627-9dbd-3c9d3225484e")]
+[assembly: Guid("06c6c10a-ab69-4c67-b717-b0bb1c764246")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
