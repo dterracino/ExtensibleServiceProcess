3621bb1019a7e62e8bb4fa669a71b9691465be0c
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-17 18:53:56 -0600

    Added a sample service module to the sample project.

---
+++ Source/ExtensibleServiceProcess.SampleConsole/SampleServiceModule.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace ExtensibleServiceProcess.SampleConsole
+{
+    public class SampleServiceModule : IServiceModule
+    {
+        public async Task OnContinueAsync()
+        {
+            await Task.FromResult(false);
+        }
+
+        public async Task OnCustomCommandAsync(int command)
+        {
+            await Task.FromResult(false);
+        }
+
+        public async Task OnPauseAsync()
+        {
+            await Task.FromResult(false);
+        }
+
+        public async Task OnShutdownAsync()
+        {
+            await Task.FromResult(false);
+        }
+
+        public async Task OnStartAsync(string[] args)
+        {
+            await Task.FromResult(false);
+        }
+
+        public async Task OnStopAsync()
+        {
+            await Task.FromResult(false);
+        }
+    }
+}
